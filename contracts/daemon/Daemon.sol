6de5181585c006896080b54a8920591ed4448a6a
Jorge Izquierdo <jorge@izqui.me>
2018-09-17 22:31:40 +0200

    Implement Daemon

---
+++ contracts/daemon/Daemon.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.4.24;
+
+import "@aragon/apps-vault/contracts/Vault.sol";
+
+
+contract Daemon {
+    function execute(uint256 payload) external {
+        require(canExecute(payload)); 
+
+        _execute(payload);
+
+        address rewardToken;
+        uint256 rewardAmount; 
+        (rewardToken, rewardAmount) = executionReward(payload);
+
+        if (rewardAmount > 0) {
+            vault().transfer(rewardToken, msg.sender, rewardAmount);
+        }
+    }
+
+    function canExecute(uint256) public view returns (bool);
+    function vault() public view returns (Vault);
+    function executionReward(uint256) public view returns (address token, uint256 amount);
+
+    function _execute(uint256) internal;
+}
